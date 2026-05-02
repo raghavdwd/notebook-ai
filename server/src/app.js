fbe91907fb79e7ab3e3682bb2b1180d667254324
Raghvendra Dwivedi <dwivediji425@gmail.com>
2026-05-02 23:57:07 +0530

    feat: implement rate limiting middleware for API requests

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import compression from "compression";
-import { rateLimit } from "express-rate-limit";
 import authRoute from "./routes/auth.route.js";
 import uploadRoute from "./routes/upload.route.js";
 import chatRoute from "./routes/chat.route.js";
@@ -15,15 +14,9 @@ import { CLIENT_APP_URL } from "../config/contants.js";
 export const app = express();
 
 //endpoint logs
+app.set("trust proxy", 1);
 app.use(helmet());
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  }),
-);
+app.use(limiter);
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(compression({ level: -1 }));
 app.use(morgan("dev"));
